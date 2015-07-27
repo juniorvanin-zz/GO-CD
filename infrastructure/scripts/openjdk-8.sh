0e0af8e6a52d313e475020a5d1484fc8afdf35a7
Junior Vanin <jvanin@thoughtworks.com>
2015-07-27 17:15:59 -0300

    remove foler gradlew

--- infrastructure/scripts/openjdk-8.sh
+++ infrastructure/scripts/openjdk-8.sh
@@ -5,5 +5,5 @@ sudo apt-get update
 sudo apt-get --yes install software-properties-common python-software-properties
 sudo add-apt-repository --yes ppa:openjdk-r/ppa
 sudo apt-get update
-sudo apt-get --yes install openjdk-7-jdk
-echo "export JAVA_HOME=/usr/lib/jvm/java-7" >> /etc/environment
+sudo apt-get --yes install openjdk-8-jdk
+echo "export JAVA_HOME=/usr/lib/jvm/java-8" >> /etc/environment
